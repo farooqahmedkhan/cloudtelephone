231e8878f18600fcdca5b820f2b5684ea6f18816
Jawad Ali <jawad.ali@repairdesk.co>
2024-03-16 12:14:15 +0500

    made navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
+import navbar from "./layouts/navbar.vue"
+
 </script>
 
 <template>
   <div class="h-screen">
+    <navbar />
     <router-view></router-view>
   </div>
 </template>
