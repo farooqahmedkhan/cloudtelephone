56ea2d05578390e04e89c991300ef254372b2c05
Jawad Ali <jawad.ali@repairdesk.co>
2024-03-16 14:22:42 +0500

    made pricing section

---
+++ src/components/Home/PricingSection.vue
@@ -0,0 +1,66 @@
+<script setup>
+import Card from "../Base/Card.vue"
+
+const businessItems = [
+  "Business telephone line",
+  "Hosted Business telephone",
+  "Business Mobile",
+  "United Conversations",
+  "VOIP SIP trunk",
+  "Landline",
+  "Broadband"
+]
+</script>
+
+<template>
+  <section class="section mt-12 bg-theme-light relative hero-1 ">
+    <div class="bg-overlay overflow-hidden bg-transparent">
+      <div class="hero-1-bg" />
+    </div>
+    <div class="container relative z-10">
+      <div class="row lg:px-10 items-center justify-center g-5">
+        <Card title="Business" class="mt-8 lg:mt-0">
+          <template #default>
+            <div class="border-border py-3 text-center">
+              <p class="mt-6">
+                Tell us more about your business requirements:
+              </p>
+              <ul class="mt-6">
+                <li v-for="item in businessItems" :key="item" class="mb-3 flex items-center justify-center font-medium">
+                  {{ item }}
+                </li>
+              </ul>
+            </div>
+          </template>
+          <template #footer>
+            <div class="text-center">
+              <router-link
+                class="btn btn-green btn-sm border-border items-center flex justify-center h-16 text-lg font-bold uppercase"
+                to="/pricing">GET YOUR PRICING</router-link>
+            </div>
+          </template>
+        </Card>
+
+        <Card title="Residential" class="mt-8 lg:mt-0">
+          <template #default>
+            <div class="border-border py-3 text-center">
+              <p class="mt-6">As low as 12 GBP per month</p>
+              <div class="py-5 my-5 text-center flex justify-center"> <img alt="" src="@/assets/images/home-service.png"
+                  width="250" height="250" class="rounded-lg">
+              </div>
+            </div>
+          </template>
+          <template #footer>
+            <div class="text-center">
+              <router-link
+                class="btn btn-purple btn-sm border-border items-center flex justify-center h-16 text-lg font-bold uppercase"
+                to="/signup">Get your QUOTE</router-link>
+            </div>
+          </template>
+        </Card>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
