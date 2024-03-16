9bdc2b8572dc03b87f9f562a73e3654d550d2b48
Jawad Ali <jawad.ali@repairdesk.co>
2024-03-16 14:22:25 +0500

    made hero section

---
+++ src/components/Home/HeroSection.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="section banner relative mt-9">
+    <div class="container">
+      <div class="row items-center">
+        <div class="lg:col-6">
+          <h1 class="banner-title">
+            <small>How does a </small>
+            <span class="block text-blue uppercase">ClOUd Based Phone</span><small>systems works</small>
+
+          </h1>
+          <p class="mt-6">
+            Easy, reliable and fairly priced phone systems designed for freelancers, startups & growing small
+            businesses.
+
+          </p>
+          <rotuer-view class="btn btn-green mt-8 cursor-pointer" to="/about">Check in Detail</rotuer-view>
+        </div>
+        <div class="lg:col-6 mt-9  flex justify-end">
+          <img class="object-contain" src="@/assets/images/main-img.png" width="580" height="396" alt="" />
+        </div>
+      </div>
+    </div>
+    <img class="banner-shape absolute -top-28 right-0 -z-[1] w-full max-w-[600px]"
+      src="@/assets/images/banner-shape.svg" alt="" />
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
