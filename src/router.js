87871d3eac699b9def982874777348b43c9b635d
Jawad Ali <jawad.ali@repairdesk.co>
2024-03-16 12:00:57 +0500

    setup pinia store for central state management

--- src/router.js
+++ src/router.js
@@ -1,10 +1,6 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
-import Home from "./pages/Home.vue"
-import About from "./pages/About.vue"
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
 ]
 
 export default createRouter( {
