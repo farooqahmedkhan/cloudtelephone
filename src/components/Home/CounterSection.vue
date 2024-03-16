879e3d8a49d278bd230c0f5e9b9fa9f91898da81
Jawad Ali <jawad.ali@repairdesk.co>
2024-03-16 15:20:45 +0500

    made counter section

---
+++ src/components/Home/CounterSection.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from "vue"
+
+const userNo = ref( "0" )
+</script>
+
+<template>
+  <section class="section relative cta flex items-center">
+    <div class="container">
+      <div class="row items-center justify-center g-5">
+        <div class="md:col-6 lg:col-12 text-center flex flex-col">
+          <h2 class="capitalize text-white">User Count</h2>
+          <p class="text-white mt-8">Easy, reliable and fairly priced phone systems designed for freelancers, startups &
+            growing small businesses.</p>
+          <div class="range mt-8">
+            {{ value }}
+            <input type="range" min="0" max="50" value="0" v-model="userNo" id="range2" class="range-input" />
+            <div class="value2 range-slider-bg">{{ userNo }} <br>USERS</div>
+          </div>
+
+        </div>
+        <div class=""><img src="@/assets/images/cta-3.png" alt="" class="absolute right-0 -bottom-10 cta-img"
+            width="530" height="396"></div>
+      </div>
+
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from "./router.js"
 import 'swiper/css'
+import 'swiper/swiper-bundle.css'
+
 
 const pinia = createPinia()
 const app = createApp( App )
