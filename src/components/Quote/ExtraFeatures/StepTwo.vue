15b63a6579c6002ad44400677d85fd39602557ff
Jawad Ali <jawad.ali@repairdesk.co>
2024-03-20 10:59:11 +0500

    divided extra-feature component into sub components

---
+++ src/components/Quote/ExtraFeatures/StepTwo.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div class="screen-4-1 pt-page ">
+    <div class="key-feature-grid  grid p-7  text-center">
+      <h4 class="text-primary">Extra Features</h4>
+      <div class="key-feature-grid grid gap-7 sm:grid-cols-2 p-7">
+        <!-- yes -->
+        <button @click="$emit('onClickYes')" class="mb-8 rounded-xl bg-theme-dark text-white py-5 px-5 shadow-lg">
+          <h5 class="h5 text-white">Yes</h5>
+          <p class="mt-4">See Pick & Mix Features</p>
+        </button>
+        <!-- no -->
+        <button @click="$emit('onClickNo')" class="mb-8 rounded-xl bg-theme-dark text-white py-5 px-5 shadow-lg">
+          <h5 class="h5 text-white">No</h5>
+          <p class="mt-2">
+            The standard setup is fine
+          </p>
+        </button>
+      </div>
+      <h5>What does this mean?</h5>
+      <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam molestie odio id
+        gravida fermentum. Etiam quis quam ante. Donec sit amet magna gravida, imperdiet ex lacinia,
+        tempor felis. Duis nec fringilla nibh. Vivamus vitae commodo sem. Vivamus vel lacinia massa.
+        Curabitur convallis sem id condimentum vulputate.</p>
+    </div>
+    <div class="w-100 flex justify-between p-7">
+      <button @click="$emit('onClickPrev')"
+        class="btn text-white rounded-full bg-theme-dark mt-10  bg-opacity-75 text-lg">Previous</button>
+    </div>
+  </div>
+</template>
---
+++ src/components/Quote/ExtraFeatures/index.vue
@@ -0,0 +1,36 @@
+<script setup>
+import StepOne from "./StepOne.vue"
+import StepTwo from "./StepTwo.vue"
+import StepThree from "./StepThree.vue"
+import { ref } from "vue"
+
+const step = ref( 1 )
+
+function onClickPrev () {
+  if ( step.value <= 1 ) {
+    window.location.hash = 'equipment'
+  } else {
+    step.value = 1
+  }
+
+}
+
+function onClickNext () {
+  if ( step.value > 1 ) {
+    window.location.hash = 'payment'
+  } else {
+    step.value++
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <StepOne v-if="step === 1" @on-click-prev="onClickPrev" @on-click-next="onClickNext" />
+    <StepTwo v-if="step === 2" @on-click-prev="onClickPrev" @on-click-yes="step = 3" @on-click-no="onClickNext" />
+    <StepThree v-if="step === 3" @on-click-prev="onClickPrev" @on-click-next="onClickNext" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
