73e1b312ef7bf3cb886d1c498946f4bf87968333
Jawad Ali <jawad.ali@repairdesk.co>
2024-04-02 23:58:31 +0500

    Add categories and fetch functionality*** ***Update props and variables in BroadbandItem.vue*** ***Reorder imports and functions in UserLinesForm/index.vue

--- src/components/Base/BroadbandItem.vue
+++ src/components/Base/BroadbandItem.vue
@@ -1,14 +1,14 @@
 <script setup>
 defineProps( {
-  title: {
+  name: {
     type: String,
     default: ""
   },
-  desc: {
+  description: {
     type: String,
     default: ""
   },
-  price: {
+  price_monthly: {
     type: Number,
     default: 0
   },
@@ -24,10 +24,10 @@ defineProps( {
     <tbody>
       <tr class="border border-neutral-200 bg-black/[0.02]  whitespace-nowrap">
         <td class="px-6 py-4" style="white-space: normal;">
-          <h6>{{ title }}</h6>
-          <p>{{ desc }}</p>
+          <h6>{{ name }}</h6>
+          <p>{{ description }}</p>
         </td>
-        <td class="px-6 py-4">${{ price }}/month</td>
+        <td class="px-6 py-4">${{ price_monthly }}/month</td>
         <td class="px-6 py-4"><input type="number" min="0" :value="modelValue"
             @input="$emit('update:modelValue', $event.target.value)" class="form-control"></td>
       </tr>
--- src/store/quotationStore.js
+++ src/store/quotationStore.js
@@ -6,6 +6,7 @@ export const useQuotationStore = defineStore( 'quotationStore', {
     return {
       userDetails: null,
       userNumbers: 0,
+      categories: [],
       broadBands: null,
       phoneTypes: 0,
       featureItems: null,
@@ -19,6 +20,22 @@ export const useQuotationStore = defineStore( 'quotationStore', {
     async createLead ( leadsData ) {
       const { data } = await axios.post( '/leads', leadsData )
       return data
+    },
+    async fetchCategories () {
+      const { data } = await axios.get( '/categories' )
+      this.categories = data.map( category => {
+        const products = category.products.map( product => {
+          return {
+            ...product,
+            value: 0
+          }
+        } )
+
+        return {
+          ...category,
+          products
+        }
+      } )
     },
     valuesSetter ( key, data ) {
       this[key] = data
