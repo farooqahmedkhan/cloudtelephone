5ddc92e7ab5b9ba697e0991063b3ebff08b7c162
Jawad Ali <jawad.ali@repairdesk.co>
2024-04-03 23:53:34 +0500

    installed vue dev tools

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
 import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig( {
-  plugins: [vue()],
+  plugins: [vue(), VueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve( __dirname, './src' ),
