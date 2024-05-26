09b1c124d11ab3a7cb9aa475a0a6cc3d61b82821
Jawad Ali <jawad.ali@repairdesk.co>
2024-05-26 10:21:33 +0500

    campaigns

---
+++ src/components/Home/CampaignSection.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { useConfigStore } from "../../store/configStore";
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Pagination } from 'swiper/modules'
+import PricingCard from "../Base/PricingCard.vue"
+
+const configStore = useConfigStore()
+</script>
+
+<template>
+  <section class="section reviews">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <swiper :slides-per-view="1" :space-between="50" :modules="[Pagination]" loop
+            :pagination="{ clickable: true }">
+            <swiper-slide v-for="campaign in configStore.campaigns" :key="campaign.id">
+              <div class="review">
+                <div v-html="campaign.content" />
+                <img v-if="campaign.image_url" :src="campaign.image_url" alt="campaign image"
+                  class="w-14 h-14 mx-auto my-2 rounded-full border-2 border-primary object-cover" />
+                <h4 class="mb-2">{{ campaign.title }}</h4>
+              </div>
+            </swiper-slide>
+          </swiper>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
--- src/store/configStore.js
+++ src/store/configStore.js
@@ -6,20 +6,24 @@ import axios from "../utils/axios.js"
 export const useConfigStore = defineStore('config-store', () => {
   const config = ref({ currency: "USD" })
   const plans = ref([])
+  const campaigns = ref([])
   // fetch settings
   async function fetchConfig() {
     try {
-      const [configResp, plansResp] = await Promise.all([
+      const [configResp, plansResp, campaignsResp] = await Promise.all([
         axios.get("/config"),
-        axios.get("/plans")
+        axios.get("/plans"),
+        axios.get("/campaigns")
       ])
       config.value = configResp.data
       plans.value = plansResp.data
+      campaigns.value = campaignsResp.data
     } catch (error) {
       console.log(error)
     }
   }
 
 
-  return { fetchConfig, config, plans }
+
+  return { fetchConfig, config, plans, campaigns }
 })
